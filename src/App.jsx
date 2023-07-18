9ff2989a6c1dd57a49c08dcad9aac3d5a47a576d
shabdan <kozhonalievshabdan@gmail.com>
2023-07-19 00:47:19 +0600

    cleared default sets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
-import './App.css'
+import './styles/App.css';
 
 function App() {
+    return(
+        <div className="App">
+            <h1 className="text-5xl bg-gray-100">bikez tailwind</h1>
+        </div>
+    )
 }
 
-export default App
+export default App;
+
