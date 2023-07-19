71dba0eb9c9a79b494f5f06cd52b71650dc6f022
shabdan <kozhonalievshabdan@gmail.com>
2023-07-19 20:02:02 +0600

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import Header from './components/Header';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
 import './styles/App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
     return(
         <div className="App">
             <Header />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/shop' element={<Shop />} />
+                <Route path='/contact' element={<Contact />} />
+                <Route path='/about' element={<About />} />
+            </Routes>
         </div>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,49 @@
 import React from "react";
 import "../styles/App.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="header bg-slate-100 py-1">
+    <div className="header bg-slate-100 py-1 relative z-10">
       <div className="header-container w-4/5 mx-auto">
         <div className="header-wrapper flex justify-between items-center">
           <div className="left">
-            <div className="logo flex">
-              <p className="logo-font text-4xl font-medium text-teal-400">
-                BIKE
-              </p>
-              <span className="logo-font text-4xl font-medium text-stone-600">
-                Z
-              </span>
+            <div className="logo">
+              <Link to='/' className="flex">
+                <p className="logo-font text-4xl font-medium text-teal-400">
+                  BIKE
+                </p>
+                <span className="logo-font text-4xl font-medium text-stone-600">
+                  Z
+                </span>
+              </Link>
             </div>
           </div>
           <div className="right flex">
             <div className="links">
-              <a href="" className="font-medium ml-3 hover:text-teal-400">
+              <Link to="/" className="font-medium ml-3 hover:text-teal-400">
                 blog
-              </a>
-              <a href="" className="font-medium ml-3 hover:text-teal-400">
+              </Link>
+              <Link to="/shop" className="font-medium ml-3 hover:text-teal-400">
                 shop
-              </a>
-              <a href="" className="font-medium ml-3 hover:text-teal-400">
+              </Link>
+              <Link
+                to="/contact"
+                className="font-medium ml-3 hover:text-teal-400"
+              >
                 about
-              </a>
-              <a href="" className="font-medium ml-3 hover:text-teal-400">
+              </Link>
+              <Link
+                to="/about"
+                className="font-medium ml-3 hover:text-teal-400"
+              >
                 contact
-              </a>
+              </Link>
             </div>
 
-            <div className="cart ml-4">
+            <div className="cart ml-4 flex">
               <svg
-                className="fill-teal-400 hover:fill-stone-600"
+                className="fill-teal-400 cursor-pointer hover:fill-teal-500"
                 viewBox="0 0 448 512"
                 width="25px"
                 height="25px"
@@ -42,6 +51,7 @@ const Header = () => {
               >
                 <path d="M416 160h-72V120C344 53.83 290.2 0 224 0S104 53.83 104 120V160H32C14.33 160 0 174.3 0 192v240C0 476.2 35.82 512 80 512h288c44.18 0 80-35.82 80-80V192C448 174.3 433.7 160 416 160zM152 120C152 80.3 184.3 48 224 48s72 32.3 72 72V160h-144V120zM128 248C114.8 248 104 237.3 104 224S114.8 200 128 200S152 210.8 152 224S141.3 248 128 248zM320 248c-13.25 0-24-10.75-24-24S306.8 200 320 200S344 210.8 344 224S333.3 248 320 248z" />
               </svg>
+              <p className="font-semibold text-xs">0</p>
             </div>
           </div>
         </div>
