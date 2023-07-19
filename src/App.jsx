5b221f836b3ece007b2d0cab25170daa5b9bff15
shabdan <kozhonalievshabdan@gmail.com>
2023-07-19 12:36:00 +0600

    setting up styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Header from './components/Header';
 import './styles/App.css';
 
 function App() {
     return(
         <div className="App">
-            <h1 className="text-5xl bg-gray-100">bikez tailwind</h1>
+            <Header />
         </div>
     )
 }
