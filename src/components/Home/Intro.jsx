9308b14b7ded05d92f10d4abe0efd2261ecd8f35
shabdan <kozhonalievshabdan@gmail.com>
2023-07-21 15:29:47 +0600

    redesigned home component

---
+++ src/components/Home/Intro.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../styles/App.css";
+
+
+const Intro = () => {
+  return (
+    <div className="home-intro h-screen w-full grid grid-cols-2 justify-center items-center relative">
+      <div className="home-link-to-shop black-lined-bg h-screen text-center flex flex-col justify-center items-center">
+        <h2 className="text-white desc-font shadowing text-3xl uppercase  z-10">
+          Adventure Awaits!
+        </h2>
+        <h2 className="text-yellow-400 desc-font shadowing text-3xl uppercase  z-10">
+          Discover New Bikes
+        </h2>
+        <h2 className="text-white desc-font shadowing text-3xl uppercase z-10">
+          in Our Shop!
+        </h2>
+        <br />
+        <Link
+          className="text-white py-2 px-6 rounded-3xl border-2 border-yellow-400 hover:bg-yellow-500 font-semibold z-10"
+          to="/shop"
+        >
+          Go to shop
+        </Link>
+      </div>
+      <img
+        src="/images/homeBikeTransparent.png"
+        alt=""
+        className="w-96 h-96 absolute left-96"
+      />
+      
+      <div className="another-one bg-[url(images/homeBikeChain.jpg)] bg-cover bg-center h-screen flex flex-col justify-center items-center">
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
