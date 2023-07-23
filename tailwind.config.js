1545f4964dead9f6e88320adba1c272b13916aeb
shabdan <kozhonalievshabdan@gmail.com>
2023-07-23 20:12:59 +0600

    updated values

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       height: {
-        '96': '29rem', 
+        96: "29rem",
+        80: "24rem",
       },
       width: {
-        '96': '29rem'
-      }
-
+        96: "29rem",
+      },
     },
   },
   plugins: [],
-}
-
+};
