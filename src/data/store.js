81998ba5b73a12e997b4b2a28838daaf4eeac79c
shabdan <kozhonalievshabdan@gmail.com>
2023-07-24 01:08:38 +0600

    created store

--- src/data/store.js
+++ src/data/store.js
@@ -1,7 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
+import product from './slices/productSlice';
+import category from './slices/categorySlice';
 
-export const store = configureStore({
+const store = configureStore({
     reducer: {
-
+        product,
+        category,
     }
-})
+});
+export default store;
