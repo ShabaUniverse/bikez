1133370bb5a82c32577513fab51cf45316d9e038
shabdan <kozhonalievshabdan@gmail.com>
2023-07-25 01:19:09 +0600

    moved categories arr to redux

--- src/data/slices/categorySlice.js
+++ src/data/slices/categorySlice.js
@@ -1,20 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    activeCategory: 0,
-}
+  activeCategory: 0,
+  categories: [
+    { name: "All", id: 0 },
+    { name: "Mountain", id: 1 },
+    { name: "Road", id: 2 },
+    { name: "Cross", id: 3 },
+    { name: "Gravel", id: 4 },
+    { name: "BMX", id: 5 },
+  ],
+};
 
 const categorySlice = createSlice({
-    name: 'category',
-    initialState,
-    reducers: {
-        setActiveCategory(state, action){
-            state.activeCategory = action.payload;
-        }
-    }
-})
+  name: "category",
+  initialState,
+  reducers: {
+    setActiveCategory(state, action) {
+      state.activeCategory = action.payload;
+    },
+  },
+});
 
 export const categorySelector = (state) => state.category;
-export const {setActiveCategory} = categorySlice.actions;
+export const { setActiveCategory } = categorySlice.actions;
 export default categorySlice.reducer;
-
