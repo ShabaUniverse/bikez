83077ac2c379056c1743a52ed026617c7d4c8ab7
shabdan <kozhonalievshabdan@gmail.com>
2023-07-25 01:21:04 +0600

    dependencies added

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Categories from "../components/Shop/Categories";
 import Products from "../components/Shop/Products";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import { productSelector } from "../data/slices/productSlice";
 import { setProducts } from "../data/slices/productSlice";
+import categorySlice, { categorySelector } from "../data/slices/categorySlice";
 
 const Shop = () => {
-  const  { products } = useSelector(productSelector);
+  const { products } = useSelector(productSelector);
+  const { activeCategory } = useSelector(categorySelector);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -15,14 +17,15 @@ const Shop = () => {
       .get("https://63d7d820afbba6b7c945b3e7.mockapi.io/bikes")
       .then((res) => {
         dispatch(setProducts(res.data));
-      })    
-  }, []);
+        console.log('category setted');
+      });
+  }, [activeCategory]);
 
   return (
     <div className="shop">
       <div className="shop-wrapper">
         <Categories />
-        {products && <Products/>}
+        {products && <Products />}
       </div>
     </div>
   );
