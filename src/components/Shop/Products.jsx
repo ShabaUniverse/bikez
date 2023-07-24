710d7f017c754cb33e424c3fb96d6fc13977d338
shabdan <kozhonalievshabdan@gmail.com>
2023-07-25 01:20:36 +0600

    now filtering is out

--- src/components/Shop/Products.jsx
+++ src/components/Shop/Products.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import ProductBox from "./ProductBox";
 import { useSelector } from "react-redux";
 import { productSelector } from "../../data/slices/productSlice";
+import { categorySelector } from "../../data/slices/categorySlice";
 
 const Products = () => {
   const { products } = useSelector(productSelector);
+  const { activeCategory, categories } = useSelector(categorySelector);
 
   return (
     <div className="products">
       <div className="products-container w-4/5 mx-auto">
         <div className="products-wrapper mt-4 grid grid-cols-3 gap-y-14">
-          {products.map((item) => (
+          {activeCategory > 0
+            ? products
+                .filter((item) => item.type.toLowerCase() === categories[activeCategory].name.toLowerCase())
+                .map((item) => <ProductBox {...item} key={item.id} />)
+            : products.map((item) => <ProductBox {...item} key={item.id} />)}
+
+          {/* {products.map((item) => (
             <ProductBox {...item} key={item.id} />
-          ))}
+          ))} */}
         </div>
       </div>
     </div>
