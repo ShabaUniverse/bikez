26e96df7856db658603f1b60816b3536e61f53cd
shabdan <kozhonalievshabdan@gmail.com>
2023-07-25 20:28:59 +0600

    create async thunk inserted

--- src/data/slices/productSlice.js
+++ src/data/slices/productSlice.js
@@ -1,18 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchProducts = createAsyncThunk(
+  "products/fetchProducts",
+  async () => {
+    const { data } = await axios.get(
+      "https://63d7d820afbba6b7c945b3e7.mockapi.io/bikes",
+    );
+    return data;
+  },
+);
 
 const initialState = {
-    products: null,
-}
+  products: null,
+  status: "",
+};
 const productSlice = createSlice({
-    name: 'product',
-    initialState: initialState,
-    reducers: {
-        setProducts(state, action){
-            state.products = action.payload;
-        },
-    }
+  name: "product",
+  initialState: initialState,
+
+  extraReducers(builder) {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.products = action.payload;
+      })
+      .addCase(fetchProducts.rejected, (state) => {
+        state.status = "failed";
+      });
+  },
 });
 
 export const productSelector = (state) => state.product;
-export const { setProducts } = productSlice.actions;
 export default productSlice.reducer;
