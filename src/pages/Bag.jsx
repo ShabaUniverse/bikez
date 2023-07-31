8b79f180a5c01a3a1e42150606d3545a7fce5b36
shabdan <kozhonalievshabdan@gmail.com>
2023-07-31 21:49:54 +0600

    created bag component

---
+++ src/pages/Bag.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Bag = () => {
+  return (
+    <div className="bag pt-12">
+        <div className="bag-container">
+            <div className="bag-wrapper">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Bag
