44914887fc93ac6637acdd971104297066650b96
shabdan <kozhonalievshabdan@gmail.com>
2023-07-31 21:50:36 +0600

    made root for bag

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import Header from './components/Header';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Home from './pages/Home';
-import Shop from './pages/Shop';
-import './styles/App.css';
-import { Routes, Route } from 'react-router-dom';
+import Header from "./components/Header";
+import About from "./pages/About";
+import Bag from "./pages/Bag";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import "./styles/App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-    return(
-        <div className="App">
-            <Header />
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/shop' element={<Shop />} />
-                <Route path='/contact' element={<Contact />} />
-                <Route path='/about' element={<About />} />
-            </Routes>
-        </div>
-    )
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/bag" element={<Bag />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
-
