760002aed069787543b717fdcd19f317b3fad691
shabdan <kozhonalievshabdan@gmail.com>
2023-08-04 13:47:49 +0600

    pre bag div

--- src/pages/Bag.jsx
+++ src/pages/Bag.jsx
@@ -7,15 +7,22 @@ import BagProduct from "../components/Bag/BagProduct";
 import BagBottom from "../components/Bag/BagBottom";
 
 const Bag = () => {
-  
+  const { bagProducts } = useSelector(bagSelector);
+
   return (
     <div className="bag pt-12">
       <div className="bag-container w-4/5 mx-auto">
-        <div className="bag-wrapper">
-          <BagTable />
-          <BagProduct />
-          <BagBottom />
-        </div>
+        {bagProducts.length > 0 ? (
+          <div className="bag-wrapper">
+            <BagTable />
+            <BagProduct />
+            <BagBottom />
+          </div>
+        ) : (
+          <div className=" h-screen absolute top-0 left-2/4 -translate-x-2/4  flex justify-center items-center">
+            <p className="text-lg font-semibold">No items in bag yet</p>
+          </div>
+        )}
       </div>
     </div>
   );
