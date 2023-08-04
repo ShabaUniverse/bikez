d7d67a6bae1fad3bdb8b642f77e6d13184a1dd7b
shabdan <kozhonalievshabdan@gmail.com>
2023-08-05 01:23:55 +0600

    almost done with that

--- src/components/Detailed/RatingStars.jsx
+++ src/components/Detailed/RatingStars.jsx
@@ -3,9 +3,24 @@ import React from "react";
 const RatingStars = ({ rating }) => {
   const stars = [];
   for (let i = 0; i < rating; i++) {
-    stars.push(<span key={i}>⭐</span>);
+    stars.push(
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        height="25px"
+        id="Layer_1"
+        version="1.1"
+        viewBox="0 0 512 512"
+        width="25px"
+        xmlSpace="preserve"
+        key={i}
+        className=" fill-yellow-400"
+        >
+        <path d="M480,207H308.6L256,47.9L203.4,207H32l140.2,97.9L117.6,464L256,365.4L394.4,464l-54.7-159.1L480,207z" />
+      </svg>,
+    );
   }
-  return <p className="">Rating: {stars}</p>;
+  return <p className="flex items-center font-medium my-1">Rating - {stars}</p>;
 };
 
 export default RatingStars;
--- src/pages/DetailedProduct.jsx
+++ src/pages/DetailedProduct.jsx
@@ -19,43 +19,85 @@ const DetailedProduct = () => {
   }, []);
 
   return (
-    <div className="detailed pt-16 bg-white">
-      <div className="detailed-container w-4/5 mx-auto">
+    <div className="pt-16 bg-white">
+      <div className="w-4/5 mx-auto">
         {detailedData && (
-          <div className="detailed-wrapper">
-            <div className="mainData detailed-grid">
-              <div className="left">
-                <img src={detailedData.imageUrl} alt="" />
+          <div className="grid grid-cols-2 gap-8">
+            <div className="col-span-1">
+              <img src={detailedData.imageUrl} alt={detailedData.title} />
+            </div>
+            <div className="col-span-1 flex flex-col justify-center pl-14">
+              <h1 className="text-2xl font-bold">{detailedData.title}</h1>
+              <p className="text-yellow-400 font-medium">{detailedData.type}</p>
+              <h2 className=" text-base font-semibold">{detailedData.desc}</h2>
+              <RatingStars rating={detailedData.rating} />
+              <div className="flex items-center">
+                <p className="font-medium mr-2">Available sizes</p>
+                {detailedData.sizes.map((size, index) => (
+                  <span
+                    key={index}
+                    className="w-8 h-8 flex items-center justify-center bg-yellow-400 text-white font-bold rounded-full mr-2">
+                    {size}
+                  </span>
+                ))}
               </div>
-              <div className="right pl-10">
-                <h1 className="font-bold text-2xl text-yellow-400">
-                  {detailedData.title}
-                </h1>
-                
-                <RatingStars rating={detailedData.rating} />
-
-                <div className="sizes flex">
-                  <p className=" font-medium mr-2">Available sizes:</p>
-                  {detailedData.sizes.map((item) => (
-                    <p className=" w-5 text-center bg-yellow-400 mr-2">
-                      {item}
-                    </p>
-                  ))}
-                </div>
-
-                <span>Discount 18%</span>
-                <h3 className=" text-2xl font-semibold text-red-500">
-                  ${detailedData.price}
-                </h3>
-                <span className=" text-green-400 font-medium">In Stock</span>
-                <br />
-                <span className=" text-green-400 font-medium">
-                  Delivery time 4-7 working days
-                </span>
-              </div>
+              <h3 className="text-2xl font-semibold">${detailedData.price}</h3>
+              <span className="text-yellow-400 font-medium">In Stock</span>
+              <span className="text-yellow-400 font-medium">
+                Delivery time: 4-7 working days
+              </span>
             </div>
           </div>
         )}
+
+        <div className="geometry">
+          <table>
+            <tr>
+              <th>Size</th>
+              <th>Rider Height</th>
+              <th>Stack</th>
+              <th>Reach</th>
+              <th>BB Drop</th>
+              <th>Wheelbase</th>
+              <th>Seat Tube Length</th>
+              <th>Top Tube Length</th>
+              <th>Head Tube Length</th>
+            </tr>
+            <tr>
+              <td>S</td>
+              <td>5'0" - 5'5" (152cm - 165cm)</td>
+              <td>530mm</td>
+              <td>380mm</td>
+              <td>70mm</td>
+              <td>1030mm</td>
+              <td>490mm</td>
+              <td>540mm</td>
+              <td>120mm</td>
+            </tr>
+            <tr>
+              <td>M</td>
+              <td>5'6" - 5'10" (168cm - 178cm)</td>
+              <td>550mm</td>
+              <td>390mm</td>
+              <td>68mm</td>
+              <td>1050mm</td>
+              <td>520mm</td>
+              <td>560mm</td>
+              <td>130mm</td>
+            </tr>
+            <tr>
+              <td>L</td>
+              <td>5'11" - 6'3" (180cm - 191cm)</td>
+              <td>570mm</td>
+              <td>410mm</td>
+              <td>66mm</td>
+              <td>1070mm</td>
+              <td>550mm</td>
+              <td>580mm</td>
+              <td>150mm</td>
+            </tr>
+          </table>
+        </div>
       </div>
     </div>
   );
