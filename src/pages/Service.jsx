adf8be9414cdbcb852993f4fa475d640e2834415
shabdan <kozhonalievshabdan@gmail.com>
2023-08-07 20:55:27 +0600

    clashing service rn

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React from "react";
+import '../styles/App.css';
 
 const Service = () => {
   return (
-    <div className="service pt-24">
-        <div className="service-container w-4/5 mx-auto">
-            <div className="service-wrapper">
-                
+    <div className="service pt-24 bg-stone-600">
+      <div className="service-container">
+        <div className="service-wrapper">
+          <div className="first-service service-grid h-96">
+            <div className="image-part h-full w-full overflow-hidden">
+                <div className="moving-image  bg-[url(images/bikeTesting.jpg)] bg-cover bg-center h-full w-full"></div>
             </div>
-        </div>
-    </div>
-  )
-}
+            <div className="desc-part bg-black"></div>
+          </div>
 
-export default Service
+          <div className="second-service service-grid h-96">
+            <div className="desc-part bg-black"></div>
+            <div className="image-part h-full w-full overflow-hidden">
+                <div className="moving-image  bg-[url(images/bikeTesting.jpg)] bg-cover bg-center h-full w-full"></div>
+            </div>
+          </div>
+
+          <div className="first-service service-grid h-96">
+            <div className="image-part h-full w-full overflow-hidden">
+                <div className="moving-image  bg-[url(images/bikeTesting.jpg)] bg-cover bg-center h-full w-full"></div>
+            </div>
+            <div className="desc-part bg-black"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
