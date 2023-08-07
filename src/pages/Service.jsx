8e2ee0e6a9df7ff6e8ff77a9556ed8ccf01dd756
shabdan <kozhonalievshabdan@gmail.com>
2023-08-07 19:32:52 +0600

    added new component

---
+++ src/pages/Service.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Service = () => {
+  return (
+    <div className="service pt-24">
+        <div className="service-container w-4/5 mx-auto">
+            <div className="service-wrapper">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Service
