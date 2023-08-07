3cdd3d6b62d26f25e6cf2440afb74bf42826c3d8
shabdan <kozhonalievshabdan@gmail.com>
2023-08-07 19:33:12 +0600

    a bit changes, wont affect

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Header from "./components/Header";
 import About from "./pages/About";
 import Bag from "./pages/Bag";
-import Contact from "./pages/Contact";
 import DetailedProduct from "./pages/DetailedProduct";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import "./styles/App.css";
 import { Routes, Route } from "react-router-dom";
 import "animate.css";
+import Service from "./pages/Service";
 
 function App() {
   return (
@@ -16,9 +16,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
         <Route path="/bag" element={<Bag />} />
+        <Route path="/service" element={<Service />} />
         <Route path={`/shop/product/:id`} element={<DetailedProduct />} />
       </Routes>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
       },
       width: {
         96: "29rem",
-      },
+      }
     },
   },
   plugins: [],
