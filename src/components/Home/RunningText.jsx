c4f50529e79ca81f3a40b1da7723025e3a3dddc4
shabdan <kozhonalievshabdan@gmail.com>
2023-08-07 13:47:03 +0600

    almost done

--- src/components/Home/RunningText.jsx
+++ src/components/Home/RunningText.jsx
@@ -4,8 +4,8 @@ const RunningText = () => {
   return (
     <div className="scrolling-wrapper bg-yellow-400 w-full h-16">
       <div className="scrolling-slider w-full flex justify-center items-center h-full">
-        <marquee behavior="alternate" direction="left" className="text-3xl text-white">
-          GIANT SCOTT CROSS COUNTRY ROAD GIANT TRINX LENON BALE PICKER SALUT
+        <marquee behavior="alternate" direction="left" className="text-3xl text-white logo-font">
+          GIANT SCOTT CROSS <span>COUNTRY</span> ROAD GIANT TRINX LENON BALE PICKER SALUT
           ASTMA HEIZELFEILD
         </marquee>
       </div>
