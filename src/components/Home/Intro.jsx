939042857ffd55e3f4694bd405f03b3331dcaabb
shabdan <kozhonalievshabdan@gmail.com>
2023-08-27 15:48:43 +0600

    restyled

--- src/components/Home/Intro.jsx
+++ src/components/Home/Intro.jsx
@@ -5,8 +5,8 @@ import "animate.css";
 
 const Intro = () => {
   return (
-    <div className="intro h-screen grid md:grid-cols-2 relative">
-      <div className="home-link-to-shop black-lined-bg hidden md:flex flex-col justify-center items-center">
+    <div className="intro h-screen grid md:grid-cols-5 relative">
+      <div className="home-link-to-shop black-lined-bg hidden md:flex flex-col justify-center items-center md:col-span-2">
         <h2 className="text-white desc-font text-2xl shadowing z-20">
           Adventure Awaits!
         </h2>
@@ -29,7 +29,7 @@ const Intro = () => {
         className="animate__animated animate__fadeInLeft hidden md:flex absolute left-1/4 top-[10%] w-96"
       />
 
-      <div className="bg-[url(/images/homewheel.jpg)] bg-cover bg-center flex justify-center items-center text-center">
+      <div className="bg-[url(/images/homewheel.jpg)] bg-cover bg-center flex justify-center items-center text-center md:col-span-3">
         <div className="right-intro-text md:hidden">
           <h2 className="text-yellow-400 desc-font text-2xl font-medium shadowing z-20">
             Adventure Awaits!
@@ -45,7 +45,7 @@ const Intro = () => {
             className="text-white text-lg font-medium desc-font animate-pulse border-2 border-yellow-400 py-2 px-5 rounded-xl z-20"
             to="/shop">
             Go to shop
-          </Link> 
+          </Link>
         </div>
       </div>
     </div>
